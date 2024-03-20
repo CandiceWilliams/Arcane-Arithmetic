f19ab9a29c2b914c6275ce83108393bdee9f201b
Justin Xu <jxu853@uwo.ca>
2024-03-20 14:50:27 -0400

    Deleted module-info.java and fixed a bug in FXML code for Sign-in/up screen

--- java/arcane_arithmetic/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.arcane.arithmetic.arcane_arithmetic {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.almasb.fxgl.all;
-
-    opens com.arcane.arithmetic to javafx.fxml;
-    exports com.arcane.arithmetic;
-}
