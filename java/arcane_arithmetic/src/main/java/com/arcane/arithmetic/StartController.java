233e642f28b1b52d211a12828c893dff8fcf5508
Ming Chun Chan <mchan624@uwo.ca>
2024-03-21 14:58:43 -0400

    StartMenu Redo v2

---
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/StartController.java
@@ -0,0 +1,11 @@
+package com.arcane.arithmetic;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.*;
+
+
+public class StartController {
+   
+}
---
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/StartMenu.java
@@ -0,0 +1,24 @@
+package com.arcane.arithmetic;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class StartMenu extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartMenu.class.getResource("view/StartMenu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        String css = this.getClass().getResource("view/StartMenu.css").toExternalForm();
+        stage.setTitle("Arcane Arithmetic");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
