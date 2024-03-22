edfea8059bf03d7b619b67ed04893edbbe29e108
Ming Chun Chan <mchan624@uwo.ca>
2024-03-22 14:45:00 -0400

    added title design

--- java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/StartMenu.java
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/StartMenu.java
@@ -13,8 +13,11 @@ public class StartMenu extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(StartMenu.class.getResource("view/StartMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         String css = this.getClass().getResource("view/StartMenu.css").toExternalForm();
+        String buttonCss = this.getClass().getResource("view/iconButton.css").toExternalForm();
         stage.setTitle("Arcane Arithmetic");
         stage.setScene(scene);
+        stage.setResizable(false);
+        
         stage.show();
     }
 
