048e8e7689fd26cc569e6ec34213a5f186e3f159
Justin Xu <jxu853@uwo.ca>
2024-03-23 18:26:23 -0400

    Updated code to work on Intellij IDE

--- java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/SignInController.java
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/SignInController.java
@@ -2,28 +2,23 @@ package com.arcane.arithmetic;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 public class SignInController {
-	
-	public Label signInLabel;
-	public Label usernameLabel;
-	public Label passwordLabel;
-	public Label dontHaveAccountLabel;
-	public Button loginButton;
-	
-	public void switchToSignUpScene(ActionEvent event) throws IOException {
-		SignInAndSignUpScreens tmp = new SignInAndSignUpScreens();
-		tmp.switchToSignUpScene(event);
-	}
-	
-	public void signInButtonClick(ActionEvent event) throws IOException {
-		
-	}
+
+    public Label signInLabel;
+    public Label usernameLabel;
+    public Label passwordLabel;
+    public Label dontHaveAccountLabel;
+    public Button loginButton;
+
+    public void switchToSignUpScene(ActionEvent event) throws IOException {
+        SignUpScreen tmp = new SignUpScreen();
+        tmp.switchToSignUpScene(event);
+    }
+
+    public void signInButtonClick(ActionEvent event) throws IOException {
+
+    }
 }
--- java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/SignUpController.java
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/SignUpController.java
@@ -2,28 +2,23 @@ package com.arcane.arithmetic;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 public class SignUpController {
-	
-	public Label signUpLabel;
-	public Label usernameLabel;
-	public Label passwordLabel;
-	public Label passwordConfirmationLabel;
-	public Button signUpButton, backToSignInScreenButton;
-	
-	public void switchToSignInScene(ActionEvent event) throws IOException {
-		SignInAndSignUpScreens tmp = new SignInAndSignUpScreens();
-		tmp.switchToSignInScene(event);
-	}
-	
-	public void signUpButtonClick(ActionEvent event) throws IOException {
-		
-	}
+
+    public Label signUpLabel;
+    public Label usernameLabel;
+    public Label passwordLabel;
+    public Label passwordConfirmationLabel;
+    public Button signUpButton, backToSignInScreenButton;
+
+    public void switchToSignInScene(ActionEvent event) throws IOException {
+        SignInScreen tmp = new SignInScreen();
+        tmp.switchToSignInScene(event);
+    }
+
+    public void signUpButtonClick(ActionEvent event) throws IOException {
+
+    }
 }
