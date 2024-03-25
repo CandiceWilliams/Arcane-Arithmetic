83b462e62d56ba563908eebce6ad1ddec0f2aaad
Ming Chun Chan <mchan624@uwo.ca>
2024-03-25 17:48:07 -0400

    added leaderboard screen

--- java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/StartController.java
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/StartController.java
@@ -24,5 +24,12 @@ public class StartController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	public void switchToLeaderboard(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("view/Leaderboard.fxml"));
+		stage = (Stage)(((Node)event.getSource()).getScene().getWindow());
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 
 }
