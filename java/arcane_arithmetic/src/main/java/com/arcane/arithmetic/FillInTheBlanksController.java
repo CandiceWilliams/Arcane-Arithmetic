3524c5a40ef66d8a5f5106c451833bc5b226a52b
Ming Chun Chan <mchan624@uwo.ca>
2024-03-28 14:11:30 -0400

    setup mulitplechoice and fillintheblanks ui screens and controller

---
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/FillInTheBlanksController.java
@@ -0,0 +1,23 @@
+package com.arcane.arithmetic;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FillInTheBlanksController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void submitAnswer(ActionEvent event){
+		
+	}
+
+}
