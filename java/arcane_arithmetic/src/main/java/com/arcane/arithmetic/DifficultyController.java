9d81dfab7a0ad45f213e90a71882a508454d8220
Justin Xu <jxu853@uwo.ca>
2024-03-29 23:58:59 -0400

    Added back the back button method in DifficultyController

--- java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/DifficultyController.java
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/DifficultyController.java
@@ -12,21 +12,24 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class DifficultyController {
-	private String diff; 
-	public void easy(ActionEvent event) throws IOException {
-		diff = "easy";
+	private String diff;
+
+	public void switchToTopic(ActionEvent event) throws IOException {
 		StartController SCCon = new StartController();
 		SCCon.switchToTopicScene(event);
+	}
+
+	public void easy(ActionEvent event) throws IOException {
+		diff = "easy";
+
 	}	
 	public void medium(ActionEvent event) throws IOException {
 		diff = "medium";
-		StartController SCCon = new StartController();
-		SCCon.switchToTopicScene(event);
+
 	}	
 	public void hard(ActionEvent event) throws IOException {
 		diff = "hard";
-		StartController SCCon = new StartController();
-		SCCon.switchToTopicScene(event);
+
 	}	
 	public String getDiff() {
 		return diff;
