b672cc56a6791a13655095a485b16f910af61c97
Ming Chun Chan <mchan624@uwo.ca>
2024-03-30 16:25:14 -0400

    module edit

--- java/arcane_arithmetic/src/main/java/module-info.java
+++ java/arcane_arithmetic/src/main/java/module-info.java
@@ -2,10 +2,11 @@ module com.arcane.arithmetic {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
-
+    
     requires com.almasb.fxgl.all;
     requires org.controlsfx.controls;
-
+	requires com.fasterxml.jackson.databind;
+    
     opens com.arcane.arithmetic to javafx.fxml;
     exports com.arcane.arithmetic;
 }
