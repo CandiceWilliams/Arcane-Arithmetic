fa46eb0bfecfe8233854ba8a8471cc3d17eeb759
Justin Xu <jxu853@uwo.ca>
2024-03-31 10:57:23 -0400

    Added getter method for public static variable 'cheatsOn'

--- java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/CheatsModePasswordPromptController.java
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/CheatsModePasswordPromptController.java
@@ -83,4 +83,8 @@ public class CheatsModePasswordPromptController {
             cheatsOffStatusLabel.setVisible(true);
         }
     }
+
+    public boolean isCheatsOn(){
+        return cheatsOn;
+    }
 }
