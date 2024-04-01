2aa0d0cd0752ffbc4095588416fe4543bb4a01a2
Candice Williams <cwill282@uwo.ca>
2024-04-01 08:09:36 -0400

    Functional fill in the blank
    Things left to add:
    - end game screen
    - countdown

--- java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/FillInTheBlanksController.java
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/FillInTheBlanksController.java
@@ -15,7 +15,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
@@ -23,6 +25,7 @@ public class FillInTheBlanksController{
 	public int num=0;
 	public int pointsNum=0;
 	private Scene scene;
+	private Stage stage;
 	@FXML private TextFlow question;
 	@FXML private BorderPane pane;
 	@FXML private Label questionNum, timeRemaining, points;
@@ -31,10 +34,13 @@ public class FillInTheBlanksController{
 	private String answer;
 	private boolean isCorrect = false;
 	GameLoop game = new GameLoop();
+	private TimerCountdown timer = new TimerCountdown();
 	private Parent root;
-	String css = this.getClass().getResource("css/fillintheblanks.css").toExternalForm();
+	//String css = this.getClass().getResource("css/fillintheblanks.css").toExternalForm();
 
-	EventHandler<ActionEvent> submitAnswer = new EventHandler<ActionEvent>() {
+
+
+	EventHandler<ActionEvent> submitButtonClick = new EventHandler<ActionEvent>() {
 		public void handle(ActionEvent e)
 		{
 			try {
@@ -44,53 +50,40 @@ public class FillInTheBlanksController{
 			}
 		}
 	};
-
-
-
-	public boolean displayQuestion(String quest, String answer) throws IOException {
-
-
+	public void displayQuestion(String quest, String answer) throws IOException {
 		question.getChildren().clear();
 		Text text = new Text(quest);
 		question.getChildren().add(text);
+		question.setTextAlignment(TextAlignment.CENTER);
+		questionNum.setText("Question "+game.getQuestionNum()+"/20");
+		points.setText("Total Points "+game.getTotalPts());
+		//timeRemaining.setText(String.valueOf(timer.countdown(60)));
+
 		this.answer = answer;
-		submitButton.setOnAction(submitAnswer);
-		return isCorrect;
+		submitButton.setOnAction(submitButtonClick);
 	}
 
-
-//	public void createQuestion(ActionEvent event) throws IOException{
-//		//Set top bar stats (question number, points)
-//		questionNum.setText("Question " + num);
-//		points.setText("Points: " + pointsNum);
-//		//Set question text
-//		Text text = new Text("QUESTION QUESTION");
-//		question.getChildren().clear();
-//		question.getChildren().add(text);
-//	}
-//	public void submitAnswer(ActionEvent event){
-//		SettingsController.settingsCon.loadSound();
-//		if(ansText.getText()==answer) {
-//
-//		}
-//	}
 
 	private void checkAnswer(ActionEvent event) throws IOException{
 		if (ansText.getText().equals(this.answer)){
 			isCorrect = true;
+			game.addPoints();
+			points.setText("Total Points "+game.getTotalPts());
+			game.correctAnswers();
 		}
 		else isCorrect = false;
 
 		game.trackQuestionNum();
+		game.StartGameLoop(event);
 	}
 
-	public void setStageAndListeners() throws IOException {
-		Stage stage = new Stage();
-		scene = new Scene(pane);
-		scene.getStylesheets().add(css);
-		stage.setScene(scene);
-		stage.show();
-
+	public Stage getStage(){
+		return stage;
 	}
+
+	public void setStage(Stage stage){
+		this.stage = stage;
+	}
+
 
 }
--- java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/GameLoop.java
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/GameLoop.java
@@ -1,42 +1,33 @@
 package com.arcane.arithmetic;
 
-import com.almasb.fxgl.time.TimerAction;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
 import java.util.Objects;
-import java.util.Scanner;
-
-import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameTimer;
 
 public class GameLoop {
 
     DifficultyController diffCon = new DifficultyController();
     TopicController topicCon = new TopicController();
-    private int questionsAnswered = 0, type_int = 1;
+    private int type_int = 1;
     private static final int TOTAL_QUESTIONS = 5;
     private String difficulty, topic, type;
     ObjectMapper objMapper = new ObjectMapper();
     Fill_Question fillQuestion = new Fill_Question();
     MultipleChoice_Question multQuestion = new MultipleChoice_Question();
-    private int totalPts = 0;
+    private static int totalPts = 0, currentQuestion = 1, correctAnswers = 0;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -48,89 +39,82 @@ public class GameLoop {
         difficulty = diffCon.getDiff();
         System.out.println(difficulty);
 
-        while(questionsAnswered != TOTAL_QUESTIONS){
+        //picks random number from 1-2 (inclusive) representing the type of question. 1 = fill in the blank 2 = multiple choice
+//        type_int  = (int)Math.floor(Math.random() * (2 - 1 + 1) + 1);
+//        if (type_int == 1){type = "fb";}
+//        if(type_int == 2){type = "mc";}
 
-            //picks random number from 1-2 (inclusive) representing the type of question. 1 = fill in the blank 2 = multiple choice
-            type_int  = (int)Math.floor(Math.random() * (2 - 1 + 1) + 1);
-            if (type_int == 1){type = "fb";}
-            if(type_int == 2){type = "mc";}
-
-            boolean isCorrect = false;
-            type_int = 1;
-            type = "fb";
-            //calls to API to store random question based on given parameters
-            String urlString = "http://127.0.0.1:5000/database/questions/get?difficulty="+difficulty+"&type="+type+"&subject="+topic;
+        boolean isCorrect = false;
+        type_int = 1;
+        type = "fb";
+        //calls to API to store random question based on given parameters
+        String urlString = "http://127.0.0.1:5000/database/questions/get?difficulty="+difficulty+"&type="+type+"&subject="+topic;
 
 
-            try {
-                URL url = new URL(urlString);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.connect();
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
 
-                //get response code
-                int responseCode = connection.getResponseCode();
-                if (responseCode != 200){
-                    throw new RuntimeException("HttpResponseCode: " + responseCode);
-                }
-
-                else{
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    StringBuilder responseStrBuilder = new StringBuilder();
-
-                    String inputStr;
-                    //Scanner scanner = new Scanner(url.openStream());
-
-                    //Write all JSON data into a string using a scanner
-                    while((inputStr = reader.readLine()) != null){
-                        responseStrBuilder.append((inputStr));
-                    }
-
-                    createQuestion(responseStrBuilder, type_int);
-
-                    reader.close();
-                    connection.disconnect();
-                }
-            }catch (IOException e) {
-                System.out.println("Unable to establish valid connection to API");
-                throw new RuntimeException(e);
+            //get response code
+            int responseCode = connection.getResponseCode();
+            if (responseCode != 200){
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
             }
 
-            if (type_int == 1){
+            else{
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder responseStrBuilder = new StringBuilder();
 
+                String inputStr;
+                //Scanner scanner = new Scanner(url.openStream());
 
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("view/FillInTheBlanks.fxml"));
-                Stage stage1 = new Stage();
-                stage1.setScene(new Scene(loader.load()));
-
-                FillInTheBlanksController fbController = loader.getController();
-
-                isCorrect = fbController.displayQuestion(fillQuestion.getQuestion(), fillQuestion.getAnswer());
-                stage1.showAndWait();
-
-
-
-                if (isCorrect){
-                    totalPts += 10;
+                //Write all JSON data into a string using a scanner
+                while((inputStr = reader.readLine()) != null){
+                    responseStrBuilder.append((inputStr));
                 }
+
+                createQuestion(responseStrBuilder, type_int);
+
+                reader.close();
+                connection.disconnect();
             }
+        }catch (IOException e) {
+            System.out.println("Unable to establish valid connection to API");
+            throw new RuntimeException(e);
+        }
 
-            if (type_int == 2){
-                root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/MultipleChoice.fxml")));
-                stage = (Stage)(((Node)event.getSource()).getScene().getWindow());
-                scene = new Scene(root);
-                stage.setScene(scene);
-                stage.show();
+        if (type_int == 1){
 
-                System.out.println(multQuestion.getQuestion());
-                System.out.println(multQuestion.getOptions());
 
-                MultipleChoiceController mcController = new MultipleChoiceController();
-                isCorrect = mcController.displayQuestion(multQuestion.getQuestion(), multQuestion.getAnswer(), multQuestion.getOptions());
-                if (isCorrect){
-                    totalPts += 10;
-                }
-            }
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("view/FillInTheBlanks.fxml"));
+            Stage stage1 = new Stage();
+            scene = new Scene(loader.load());
+            scene.getStylesheets().add("../css/fillintheblanks.css");
+            stage1.setScene(scene);
+
+
+            FillInTheBlanksController fbController = loader.getController();
+            fbController.setStage(stage1);
+            stage1.show();
+            fbController.displayQuestion(fillQuestion.getQuestion(), fillQuestion.getAnswer());
+
+        }
+
+        if (type_int == 2){
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/MultipleChoice.fxml")));
+            stage = (Stage)(((Node)event.getSource()).getScene().getWindow());
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+            System.out.println(multQuestion.getQuestion());
+            System.out.println(multQuestion.getOptions());
+
+            MultipleChoiceController mcController = new MultipleChoiceController();
+            //isCorrect = mcController.displayQuestion(multQuestion.getQuestion(), multQuestion.getAnswer(), multQuestion.getOptions());
+
         }
     }
 
@@ -140,6 +124,7 @@ public class GameLoop {
             String question = json.at("/question").toString();
             String answer = json.at("/answer").toString();
             String id = json.at("/question_id").toString();
+            String subject = json.at("/subject").toString();
 
             question = question.substring(1, question.length() - 1);
             answer = answer.substring(1, answer.length() - 1);
@@ -147,6 +132,9 @@ public class GameLoop {
             fillQuestion.setQuestion(question);
             fillQuestion.setAnswer(answer);
             fillQuestion.setQuestion_id(Integer.parseInt(id));
+
+            System.out.println(question);
+            System.out.println(answer);
         }
 
         if (type == 2){  //multiple choice question format
@@ -168,7 +156,27 @@ public class GameLoop {
     }
 
     public void trackQuestionNum(){
-        questionsAnswered++;
+        currentQuestion++;
+    }
+
+    public void addPoints(){
+        totalPts += 10;
+    }
+
+    public void correctAnswers(){
+        correctAnswers++;
+    }
+
+    public int getQuestionNum(){
+        return currentQuestion;
+    }
+
+    public int getTotalPts(){
+        return totalPts;
+    }
+
+    public int getCorrectAnswers(){
+        return correctAnswers;
     }
 
 
