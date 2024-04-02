cd55dcabc9d504ffacd9ff62913029cdff785f22
Ming Chun Chan <mchan624@uwo.ca>
2024-04-01 22:39:28 -0400

    difficulty javadoc

--- java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/DifficultyController.java
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/DifficultyController.java
@@ -13,7 +13,10 @@ import javafx.stage.Stage;
 
 /**
  * This is the Difficulty Controller, used for controls and events in the ChooseDifficulty scene.
- *
+ * 
+ * @author Ming Chun Chan
+ * @author Justin Xu
+ * @author Candice Williams
  */
 public class DifficultyController {
 	private static String diff;
@@ -22,7 +25,7 @@ public class DifficultyController {
 	/**
 	 * Switch back to the previous scene, ChooseTopic scene
 	 * @param event the back button is pressed
-	 * @throws IOException
+	 * @throws IOException if error initialising Stage variable
 	 */
 	public void switchToTopic(ActionEvent event) throws IOException {
 		StartController SCCon = new StartController();
@@ -33,7 +36,7 @@ public class DifficultyController {
 	 * Save easy as the difficulty
 	 * Start the game
 	 * @param event the easy button is pressed
-	 * @throws IOException
+	 * @throws IOException if error initialising Stage variable
 	 */
 	public void easy(ActionEvent event) throws IOException {
 		diff = "easy";
@@ -44,7 +47,7 @@ public class DifficultyController {
 	 * Save medium as the difficulty
 	 * Start the game
 	 * @param event the medium button is pressed
-	 * @throws IOException
+	 * @throws IOException if error initialising Stage variable
 	 */
 	public void medium(ActionEvent event) throws IOException {
 		diff = "medium";
@@ -54,7 +57,7 @@ public class DifficultyController {
 	 * Save hard as the difficulty
 	 * Start the game
 	 * @param event the hard button is pressed
-	 * @throws IOException
+	 * @throws IOException if error initialising Stage variable
 	 */
 	public void hard(ActionEvent event) throws IOException {
 		diff = "difficult";
@@ -70,7 +73,7 @@ public class DifficultyController {
 	/**
 	 * Start the game
 	 * @param event any of the three difficulty buttons is pressed
-	 * @throws IOException
+	 * @throws IOException if error initialising Stage variable
 	 */
 	public void startGame(ActionEvent event) throws IOException {
 		GameLoop game = new GameLoop();
