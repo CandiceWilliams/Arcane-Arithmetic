3d3bb04cb5b65d06a53fa963ee85c0e4250364ea
Candice Williams <cwill282@uwo.ca>
2024-04-01 21:29:30 -0400

    StartMenu Javadocs created

--- java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/StartMenu.java
+++ java/arcane_arithmetic/src/main/java/com/arcane/arithmetic/StartMenu.java
@@ -8,11 +8,35 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 /**
- * This is the start menu, used for loading the game.
- *
+ * This is the main game window that the user will see when first starting the game.
+ * It extends the <a href="#{@Application}">{@link Application}</a> class.
+ * <br></br>
+ * <br></br>
+ * @author Arian Firoozfar
+ * @author Candice Williams
+ * @author Jamie Muha
+ * @author Justin Xu
+ * @author Ming Chun Chan
+ * @version 1.1.0
+ * @see Application
  */
 public class StartMenu extends Application {
 
+    /**
+     * Start loads the start menu which functions as the main game window. User will have access to:
+     * <li>the tutorial,</li>
+     * <li>the options menu,</li>
+     * <li>the sign-in/sign-up popup</li>
+     * <li>the exit button and,</li>
+     * <li>the start game button</li>
+     *
+     * @author Ming Chun Chan
+     * @author Candice Williams
+     * @author Justin Xu
+     * @param stage the primary stage used to display main game window
+     * @throws IOException If error occured when loading FXML file.
+     * @see javafx.fxml.FXML
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartMenu.class.getResource("view/StartMenu.fxml"));
@@ -25,6 +49,11 @@ public class StartMenu extends Application {
         CheatsModePasswordPromptController.cheatsCon.initCheats();
     }
 
+    /**
+     * Main menu that launches the JavaFX game
+     * @author Ming Chun Chan
+     * @param args argument from cmd
+     */
     public static void main(String[] args) {
         launch();
     }
